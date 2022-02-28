f178c2e0fda8faa7953cc30308b1f3ab897c4703
selentia <admin@duna.me>
2022-02-28 21:28:23 +0900

    ^^7

--- google_sheet.js
+++ google_sheet.js
@@ -22,7 +22,7 @@ async function execute(client) {
     // 1. 시트 읽기
     const sheetserach = {
         'spreadsheetId': 'yoursheetid',
-        'range': "sheet!range" //시트이름!셀
+        'range': 'sheet!range' //시트이름!셀
     };
 
     // 2. 시트 복제
@@ -41,12 +41,12 @@ async function execute(client) {
 
     // 3. 시트 내용 등록
     const Array1 = new Array();
-    Array1[0] = new Array("연번", "이름", "포지션", "포지션", "방어타입", "소속학원");
-    Array1[1] = new Array("1", "리쿠하치마 아루", "딜러", "폭발", "경장갑", "게헨나");
-    Array1[2] = new Array("2", "소라사키 히나", "딜러", "폭발", "중장갑", "게헨나");
-    Array1[3] = new Array("3", "타카나시 호시노", "탱커", "관통", "중장갑", "아비도스");
-    Array1[4] = new Array("4", "텐도 아리스", "딜러", "신비", "특수장갑", "밀레니엄");
-    Array1[5] = new Array("5", "쿠다 이즈나", "딜러", "신비", "경장갑", "백귀야행");
+    Array1[0] = new Array('연번', '이름', '포지션', '포지션', '방어타입', '소속학원');
+    Array1[1] = new Array('1', '리쿠하치마 아루', '딜러', '폭발', '경장갑', '게헨나');
+    Array1[2] = new Array('2', '소라사키 히나', '딜러', '폭발', '중장갑','게헨나');
+    Array1[3] = new Array('3', '타카나시 호시노', '탱커', '관통', '중장갑', '아비도스');
+    Array1[4] = new Array('4', '텐도 아리스', '딜러', '신비', '특수장갑', '밀레니엄');
+    Array1[5] = new Array('5', '쿠다 이즈나', '딜러', '신비', '경장갑', '백귀야행');
 
     const sheetappend = {
         spreadsheetId: sheetid,
